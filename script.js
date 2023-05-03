f5d89c7147a6ec8412178b51f2da42db0042d293
kumargunjansingh <gunjansinghji21@gmail.com>
2023-05-04 00:20:36 +0530

    WeatherApp

--- script.js
+++ script.js
@@ -1,46 +1,35 @@
-const inputBox = document.querySelector('.input-box');
-const searchBtn = document.getElementById('searchBtn');
-const weather_img = document.querySelector('.weather-img');  
+const inputBox = document.querySelector('.input_box');
+const searchBtn = document.getElementById('searchBtn');  
+const weather_img = document.querySelector('.weather_img');   
 const temperature = document.querySelector('.temperature');
-const description = document.querySelector('.description')
+const description = document.querySelector('.description'); 
 const humidity = document.getElementById('humidity');
-const wind_speed = document.getElementById('wind-speed') ;
-
-const location_not_found = document.querySelector('.location-not-found');
+const wind_speed = document.getElementById('wind_speed') ;
+const location_not_found = document.querySelector('.location_not_found');
 const weather_body = document.querySelector('.weather_body');  
-
-
-async function checkWeather(city)
-{
+async function checkWeather(city)                                                                            
+{ 
    const api_key ="128fa806702b2f7916f22a5ac1d8f926"; 
-
-   const url =  'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}'; 
-
-
-
-
-   const weather_data = await fetch('${url}').then(response => response.json());
-
-     if(weather_data.cod === '404')
+   const url =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;                  
+  const weather_data = await fetch(`${url}`).then(response => response.json());
+     if(weather_data.cod === `404`)
      { 
-        location_not_found.style.display = "flex";
+        location_not_found.style.display = "flex";  
         weather_body.style.display = "none";
         console.log("error");
         return;
      } 
-     
+     console.log("run");   
      location_not_found.style.display = "none";
      weather_body.style.display = "flex";
-     temperature.innerHTML = '${Math.round(Weather_data.main.temp-273.15)}°C';
+     temperature.innerHTML = `${Math.round(weather_data.main.temp-273.15)}°C`;
 
-     description.innerHTML = '${weather_data.weather[0].description}';
+     description.innerHTML = `${weather_data.weather[0].description}`;
 
-     humidity.innerHTML = '${weather_data.main.humidity}%';
-     wind_speed.innerHTML = '${weather_data.wind.speed}km/H';  
-
-
+     humidity.innerHTML = `${weather_data.main.humidity}%`;
+     wind_speed.innerHTML = `${weather_data.wind.speed}km/H`;
        switch(weather_data.weather[0].main)
-       {
+       { 
          case 'clouds':  
          weather_img.scr = "/photos/cloud.png";
          break;
@@ -48,7 +37,7 @@ async function checkWeather(city)
          weather_img.scr = "/photos/clear.png";
          break;
          case 'Rain':  
-         weather_img.scr = "/photos/rain.png";
+         weather_img.scr ="/photos/rain.png";
          break;
          case 'Mist':  
          weather_img.scr = "/photos/mist.png";
@@ -56,15 +45,9 @@ async function checkWeather(city)
          case 'Snow':  
          weather_img.scr = "/photos/snow.png";
          break;
-         
-
-       }
-
+       }                                              
      console.log(weather_data);
-
 }
-
-
-searchBtn.addEventListener('click',()=>{
-    checkWeather(inputBox.value)
-});
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(inputBox.value);
+});        
